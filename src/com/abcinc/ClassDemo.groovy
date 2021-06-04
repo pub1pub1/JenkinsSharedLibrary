d29dd55a0a6c0cfd973d3d207da4ba49a2e42abf
Bobby <bobby@abcinc.com>
2021-06-04 13:52:26 -0400

    class

--- src/com/abcinc/ClassDemo.groovy
+++ src/com/abcinc/ClassDemo.groovy
@@ -10,39 +10,48 @@ class ClassDemo implements Serializable {
   public checkout() {
     script.node() {
     script.stage('Checkout') {
-      script.git ''
+      script.git 'https://github.com/pub1pub1/SimpleGreetingMaven.git'
       script.echo 'Checkout done'
     }
   }
   }
 
-  // public stage_package() {
-  //   script.stage('Package') {
-  //     script.stage_package()
-  //   }
-  // }
+  public stage_package() {
+    script.stage('Package') {
+      script.echo 'Package'
+    }
+  }
 
-  // public stage_clean() {
-  //   script.stage('Clean') {
-  //     script.stage_clean()
-  //   }
-  // }
+  public stage_clean() {
+    script.stage('Clean') {
+      script.echo 'clean'
+    }
+  }
 
-  // public stage_sit() {
-  //   script.stage('SIT') {
-  //     script.stage_sit()
-  //   }
-  // }
+  public stage_sit() {
+    script.stage('SIT') {
+      script.echo 'SIT'
+    }
+  }
 
-  // public stage_archive_reports() {
-  //   script.stage('Archive Reports') {
-  //     script.stage_archive_reports()
-  //   }
-  // }
+  public stage_archive_reports() {
+    script.stage('Archive Reports') {
+      script.echo 'Archive Reports'
+    }
+  }
 
-  // public stage_uat(job) {
-  //   script.stage('UAT') {
-  //     script.stage_uat()
-  //   }
-  // }
+  public stage_uat(job) {
+    script.stage('UAT') {
+    
+    def response= script.input message: 'Is this build good to go?',
+         parameters: [choice(choices: 'Yes\nNo', 
+         description: '', name: 'Pass')]
+
+    if(response=="Yes") {
+        script.stage('Deploy') {
+        // bat "xcopy \"C:\\Program Files (x86)\\Jenkins\\workspace\\$job\\target\\SimpleGreeting*.jar\" C:\\workspace\\dev\\ /y"
+          script.echo 'Deployed!'
+         }    
+    }
+  }
 }
